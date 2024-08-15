cd5035b86ceee57577ee1afc8cf26d081a2d378b
AlexisGadbin <alexis@gadbin.com>
2024-08-15 21:30:09 +0200

    feat: add note to book

--- src/components/books/book-list.tsx
+++ src/components/books/book-list.tsx
@@ -1,8 +1,10 @@
 import { getBooks } from '@/api/services/book.service'
 import { useQuery } from '@tanstack/react-query'
+import { useTranslation } from 'react-i18next'
 import BookCard from './book-card'
 
 const BooksList = () => {
+  const { t } = useTranslation()
   const { data: paginatedBooks } = useQuery({
     queryKey: ['books'],
     queryFn: getBooks,
@@ -13,11 +15,14 @@ const BooksList = () => {
   }
 
   return (
-    <section className="flex gap-6 overflow-x-auto pb-4">
-      {paginatedBooks.data.map((book) => (
-        <BookCard key={book.id} book={book} />
-      ))}
-    </section>
+    <>
+      <h2>{t('home.books_list_title')}</h2>
+      <section className="flex gap-6 overflow-x-auto pb-4">
+        {paginatedBooks.data.map((book) => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </section>
+    </>
   )
 }
 
